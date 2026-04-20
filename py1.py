61a152328bd67b7d5263eb4df480f578f12af7f0
akshitthakur1 <akshitthakur10790@gmail.com>
2026-04-20 23:22:22 +0530

    Delete py1.py

--- py1.py
+++
@@ -1,9 +0,0 @@
-#print all prime numbers in an interval
-a = int(input("enter the  number;"))
-for i in range(1,a):
-    for j in range(2,i):
-        if i%j==0:
-            print(i)
-            break
-
-
